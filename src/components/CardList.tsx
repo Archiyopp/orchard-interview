7f4a09dbcdad81ff64dc0ee040fb70e6367957f0
Archiyopp <crisfr97@gmail.com>
2025-02-08 01:37:14 -0500

    add webp images using source element

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,6 +1,9 @@
 import Red from "../assets/red.png";
 import Green from "../assets/green.png";
 import White from "../assets/white.png";
+import WhiteWebP from "../assets/white.webp";
+import RedWebP from "../assets/red.webp";
+import GreenWebP from "../assets/green.webp";
 
 export function CardList() {
     return (
@@ -12,10 +15,19 @@ export function CardList() {
     );
 }
 
-function Card({ name, description, imageURL, alt }: CardData) {
+function Card({ name, description, imageURL, alt, imageWebP }: CardData) {
     return (
         <li className="flex flex-col gap-7.5 max-w-93.75">
-            <img src={imageURL} alt={alt} height={300} width={375} />
+            <picture>
+                <source srcSet={imageWebP} type="image/webp" />
+                <img
+                    src={imageURL}
+                    alt={alt}
+                    height={300}
+                    width={375}
+                    className="object-cover h-75 w-full"
+                />
+            </picture>
             <div className="flex flex-col gap-2.5 px-7.5">
                 <h2 className="text-[1.3125rem] leading-7.5 font-bold text-center">
                     {name}
@@ -30,6 +42,7 @@ type CardData = {
     name: string;
     description: string;
     imageURL: string;
+    imageWebP: string;
     alt: string;
 };
 
@@ -39,6 +52,7 @@ const cardListData: Array<CardData> = [
         description:
             "Red foods remind us of berries and soft fruits, so we anticipate a sweet taste.",
         imageURL: Red,
+        imageWebP: RedWebP,
         alt: "Red food",
     },
     {
@@ -46,6 +60,7 @@ const cardListData: Array<CardData> = [
         description:
             "Fresh, zingy green colours are reminiscent of unripe fruit, promising sour or acid flavours.",
         imageURL: Green,
+        imageWebP: GreenWebP,
         alt: "Green food",
     },
     {
@@ -53,6 +68,7 @@ const cardListData: Array<CardData> = [
         description:
             "White foods evoke memories of salt and salty flavours, driving the expectation of a savoury treat. ",
         imageURL: White,
+        imageWebP: WhiteWebP,
         alt: "White food",
     },
 ];
